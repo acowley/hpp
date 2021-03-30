c5292a08a6ca6922c674b0b6da0a79c26e59dbee
Anthony Cowley <acowley@gmail.com>
2021-03-30 09:51:40 -0400

    Bump pinned nixpkgs; simplify shell.nix updates
    I usually want to run with whatever version of GHC is current in the
    pinned nixpkgs. Manually updating that version is no longer needed.

--- shell.nix
+++ shell.nix
@@ -1,16 +1,16 @@
-{ compiler ? "ghc883"
+{ compiler ? null
 , withHoogle ? true
 , sources ? import ./nix/sources.nix
 }:
 let
-  pkgs = import sources.nixpkgs {};
-  hspkgs = pkgs.haskell.packages.${compiler};
+  pkgs = import sources.nixpkgs-chan {};
+  hspkgs = if isNull compiler then pkgs.haskellPackages else pkgs.haskell.packages.${compiler};
   drv = hspkgs.callPackage ./default.nix {};
-  ghc = hspkgs.ghc.withHoogle (ps: drv.passthru.getBuildInputs.haskellBuildInputs ++ [ps.cabal-install]);
+  ghc = hspkgs.ghc.withHoogle (ps: drv.passthru.getBuildInputs.haskellBuildInputs);
 in pkgs.mkShell {
-  buildInputs = [ ghc ];
-  shellHook = ''
-    source <(grep '^export NIX_' ${ghc}/bin/ghc)
-    source <(echo 'export HIE_HOOGLE_DATABASE='$(grep -F -- '--database' ${ghc}/bin/hoogle | sed 's/.* --database \(.*\.hoo\).*/\1/'))
-  '';
+  buildInputs = [ ghc hspkgs.cabal-install hspkgs.haskell-language-server];
+  # shellHook = ''
+  #   source <(grep '^export NIX_' ${ghc}/bin/ghc)
+  #   source <(echo 'export HIE_HOOGLE_DATABASE='$(grep -F -- '--database' ${ghc}/bin/hoogle | sed 's/.* --database \(.*\.hoo\).*/\1/'))
+  # '';
 }
